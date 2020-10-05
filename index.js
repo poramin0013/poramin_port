f2f34a1378ea41b689d28c90b56ba8b092bb40f1
anurak klakphet <604259014@webmail.npru.ac.th>
2020-10-05 11:42:21 +0700

    Initial portfolio project Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./routes/index');
 const hbs = require('express-handlebars');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,10 @@ const router = express. Router();
 const myData = require('../data')
 
 router.get('/',(req,res)=>{
-    res.render('index',{myData});
+    res.render('index');
 });
 
-/*router.get('/',(req, res )=>{
-res.send( "<h1>My Portfolio</h1>");
-});*/
 
-router.get('/aboutme', (req, res )=>{
-    res.render('about',{myData});
-});
 
-router.get('/education', (req,res )=>{
-    res.render('education',{myData});
-});
 
-router.get('/workexperience',(req, res )=>{
-    res.render('experience',{myData});
-});
-
-router.get('/skill', (req, res )=>{
-    res.render('skill',{myData});
-});
-
-router.get('/contact', (req, res )=>{
-    res.render('contact',{myData});
-});
 module.exports = router;
